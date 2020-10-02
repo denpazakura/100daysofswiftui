95ad28d19f6a4e4a08d04f923162a37a2aac0379
denpazakura <swiftthesorrow@gmail.com>
2020-10-02 17:13:22 +0200

    Day 21 wrap up

--- Flags/Flags/ContentView.swift
+++ Flags/Flags/ContentView.swift
@@ -8,17 +8,22 @@
 
 import SwiftUI
 
+let color1 = Color(red: 233/255.0, green: 241/255.0, blue: 242/255.0)
+let color2 = Color(red: 189/255.0, green: 200/255.0, blue: 216/255.0)
+
 struct ContentView: View {
-    
     @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"]
     @State private var correctAnswer = Int.random(in: 0...2)
     
     @State private var showingScore = false
     @State private var scoreTitle = ""
     
+    @State private var currentScore = 0
+    
     var body: some View {
+        
         ZStack {
-            LinearGradient(gradient: Gradient(colors: [.blue, .black]), startPoint: .top, endPoint: .bottom)
+            LinearGradient(gradient: Gradient(colors: [color1, color2]), startPoint: .top, endPoint: .bottom)
                 .edgesIgnoringSafeArea(.all)
             VStack(spacing: 30) {
                 VStack {
@@ -34,7 +39,7 @@ struct ContentView: View {
                     Button(action: {
                         self.flagTapped(number)
                     }) {
-                        Image(self.countries[number])
+                        Image(self.countries[number].lowercased())
                             .renderingMode(.original)
                             .clipShape(Capsule())
                             .overlay(Capsule().stroke(Color.black, lineWidth: 1))
@@ -42,10 +47,14 @@ struct ContentView: View {
                     }
                     Spacer()
                 }
+                
+                Text("current score: \(currentScore)")
+                    .foregroundColor(.white)
+                    .font(.largeTitle)
             }
                 
             .alert(isPresented: $showingScore) {
-                Alert(title: Text(scoreTitle), message: Text("Your score is ???"), dismissButton: .default(Text("Continue")) {
+                Alert(title: Text(scoreTitle), message: Text("Your score is \(currentScore)"), dismissButton: .default(Text("Continue")) {
                     self.askQuestion()
                     })
             }
@@ -55,8 +64,10 @@ struct ContentView: View {
     private func flagTapped(_ number: Int) {
         if number == correctAnswer {
             scoreTitle = "Correct"
+            addPoints()
         } else {
-            scoreTitle = "Wrong"
+            deduct()
+            scoreTitle = "Wrong, that's \(countries[number])"
         }
         
         showingScore = true
@@ -66,10 +77,19 @@ struct ContentView: View {
         countries.shuffle()
         correctAnswer = Int.random(in: 0...2)
     }
+    
+    private func addPoints() {
+        currentScore += 1
+    }
+    
+    private func deduct() {
+        guard currentScore != 0 else {
+            return
+        }
+        
+        currentScore -= 1
+    }
 }
-
-
-
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
