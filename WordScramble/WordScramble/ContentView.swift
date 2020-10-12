f117b2288661aae866f04b38fd25ade53c823e3b
denpazakura <swiftthesorrow@gmail.com>
2020-10-12 23:55:52 +0200

    Day 32(refactoring WordScramble)

--- WordScramble/WordScramble/ContentView.swift
+++ WordScramble/WordScramble/ContentView.swift
@@ -36,9 +36,9 @@ struct ContentView: View {
             .navigationBarItems(leading: Button("Start", action: {
                 self.startGame()
             }), trailing: Text("Current  score: \(currentScore)"))
-            .onAppear(perform: startGame)
-            .alert(isPresented: $showingError) {
-                Alert(title: Text(errorTitle), message: Text(errorMessage), dismissButton: .default(Text("OK")))
+                .onAppear(perform: startGame)
+                .alert(isPresented: $showingError) {
+                    Alert(title: Text(errorTitle), message: Text(errorMessage), dismissButton: .default(Text("OK")))
             }
         }
     }
@@ -52,35 +52,17 @@ private extension ContentView {
             return
         }
         
-        guard isOriginal(word: answer) else {
-            wordError(title: "Word used already", message: "Be more original")
-            return
-        }
-
-        guard isPossible(word: answer) else {
-            wordError(title: "Word not recognized", message: "You can't just make them up, you know!")
-            return
-        }
-
-        guard isReal(word: answer) else {
-            wordError(title: "Word not possible", message: "That isn't a real word.")
-            return
-        }
+        let wordChecker = WordChecker(dictionary: usedWords, rootWord: rootWord)
         
-        guard isNotShort(word: answer) else {
-            wordError(title: "word too short", message: "Your answer must contain at least 3 characters")
+        let result = wordChecker.isValid(word: answer)
+        
+        guard let validationError = result.filter({ $0.1 != nil }).first else {
+            usedWords.insert(answer, at: 0)
+            currentScore += answer.count
+            newWord = ""
             return
         }
-        
-        guard isNotRoot(word: answer) else {
-            wordError(title: "this word is the same as the start word", message: "Please type another word")
-            return
-        }
-        
-        usedWords.insert(answer, at: 0)
-        
-        currentScore += answer.count
-        newWord = ""
+        wordError(title: validationError.1?.localizedDescription ?? "", message: "")
     }
     
     func startGame() {
@@ -92,40 +74,6 @@ private extension ContentView {
             }
         }
         fatalError("Could not load start.txt from bundle.")
-    }
-    
-    func isOriginal(word: String) -> Bool {
-        !usedWords.contains(word)
-    }
-    
-    func isPossible(word: String) -> Bool {
-        var tempWord = rootWord
-
-        for letter in word {
-            if let pos = tempWord.firstIndex(of: letter) {
-                tempWord.remove(at: pos)
-            } else {
-                return false
-            }
-        }
-
-        return true
-    }
-    
-    func isReal(word: String) -> Bool {
-        let checker = UITextChecker()
-        let range = NSRange(location: 0, length: word.utf16.count)
-        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
-
-        return misspelledRange.location == NSNotFound
-    }
-    
-    func isNotShort(word: String) -> Bool {
-        return word.count >= 3
-    }
-    
-    func isNotRoot(word: String) -> Bool {
-        return word != rootWord
     }
     
     func wordError(title: String, message: String) {
---
+++ WordScramble/WordScramble/WordValidator.swift
@@ -0,0 +1,100 @@
+//
+//  WordValidator.swift
+//  WordScramble
+//
+//  Created by denpazakura on 12/10/2020.
+//  Copyright © 2020 denpazakura. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+enum WordValidationError: Error {
+    case wordNotOriginal
+    case wordNotPossible
+    case wordNotReal
+    case wordTooShort
+    case wordIsRoot
+}
+
+extension WordValidationError {
+    var localizedDescription: String {
+        switch self {
+        case .wordNotOriginal: return "Word used already"
+        case .wordNotPossible: return "Word not recognized"
+        case .wordNotReal: return "Word not possible "
+        case .wordTooShort: return "This word is too short"
+        case .wordIsRoot: return "This word is the same as the start word"
+        }
+    }
+}
+
+protocol WordValidator {
+    func isValid(word: String) -> [(Bool, WordValidationError?)]
+}
+
+class WordChecker: WordValidator {
+    var usedWords = [String]()
+    
+    private var dictionary: [String]
+    private var rootWord: String
+    
+    private var validators = Array<((Bool, WordValidationError?))>()
+    
+    init(dictionary: [String], rootWord: String) {
+        self.dictionary = dictionary
+        self.rootWord = rootWord
+    }
+    
+    
+    func setupValidationRules(word: String) {
+        validators.append(isOriginal(word: word))
+        validators.append(isPossible(word: word))
+        validators.append(isReal(word: word))
+        validators.append(isNotRoot(word: word))
+        validators.append(isNotShort(word: word))
+    }
+    
+    func isValid(word: String) -> [(Bool, WordValidationError?)] {
+        setupValidationRules(word: word)
+        
+        return validators.map { ($0.0, $0.1) }
+    }
+}
+
+private extension WordChecker {
+    func isOriginal(word: String) -> (Bool, WordValidationError?) {
+        return !usedWords.contains(word) ? (true, nil) : (false, .wordNotOriginal)
+    }
+    
+    func isPossible(word: String) -> (Bool, WordValidationError?) {
+        var tempWord = rootWord
+        
+        for letter in word {
+            if let pos = tempWord.firstIndex(of: letter) {
+                tempWord.remove(at: pos)
+            } else {
+                return (false, .wordNotPossible)
+            }
+        }
+        
+        return (true, nil)
+    }
+    
+    func isReal(word: String) -> (Bool, WordValidationError?) {
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        
+        return misspelledRange.location == NSNotFound ? (true, nil) : (true, .wordNotReal)
+        
+    }
+    
+    func isNotShort(word: String) -> (Bool, WordValidationError?) {
+        return word.count >= 3 ? (true, nil) : (false, .wordTooShort)
+    }
+    
+    func isNotRoot(word: String) -> (Bool, WordValidationError?) {
+        return word != rootWord ? (true, nil) : (false, .wordIsRoot)
+    }
+}
