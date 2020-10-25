f3a3f8b759830fb5a13c392e534a731475daff8a
denpazakura <swiftthesorrow@gmail.com>
2020-10-25 23:45:55 +0100

    Day 45

---
+++ Drawing/Drawing/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  Drawing
+//
+//  Created by denpazakura on 25/10/2020.
+//  Copyright © 2020 denpazakura. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
---
+++ Drawing/Drawing/Trapezoid.swift
@@ -0,0 +1,25 @@
+//
+//  Trapezoid.swift
+//  Drawing
+//
+//  Created by denpazakura on 25/10/2020.
+//  Copyright © 2020 denpazakura. All rights reserved.
+//
+
+import SwiftUI
+
+struct Trapezoid: Shape {
+    var insetAmount: CGFloat
+
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+
+        path.move(to: CGPoint(x: 0, y: rect.maxY))
+        path.addLine(to: CGPoint(x: insetAmount, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX - insetAmount, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: 0, y: rect.maxY))
+
+        return path
+   }
+}
