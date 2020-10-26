9dbc5b5cfb7a419f92591e9384cb809261257f38
denpazakura <swiftthesorrow@gmail.com>
2020-10-26 16:07:31 +0100

    mend

---
+++ Spirograph/Spirograph/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  Spirograph
+//
+//  Created by denpazakura on 26/10/2020.
+//  Copyright © 2020 denpazakura. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
