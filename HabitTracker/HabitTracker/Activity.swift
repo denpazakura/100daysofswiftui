37ee247c8c3f3fb0d550be6dfc9baa72a61667e6
denpazakura <swiftthesorrow@gmail.com>
2020-10-29 21:20:27 +0100

    Day 48 (Day 47 project)

---
+++ HabitTracker/HabitTracker/Activity.swift
@@ -0,0 +1,29 @@
+//
+//  Activity.swift
+//  HabitTracker
+//
+//  Created by denpazakura on 29/10/2020.
+//  Copyright © 2020 denpazakura. All rights reserved.
+//
+
+import SwiftUI
+
+struct Activity: Codable, Identifiable {
+    let name: String
+    let description: String
+    
+    let id: Int
+    let date: Date?
+}
+
+extension Activity {
+    var formattedDate: String? {
+        if let date = date {
+            let formatter = DateFormatter()
+            formatter.dateStyle = .long
+            return formatter.string(from: date)
+        } else {
+            return nil
+        }
+    }
+}
