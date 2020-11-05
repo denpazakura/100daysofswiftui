d5a62804b93e3fb039375eb8e93fc509033dcc66
denpazakura <swiftthesorrow@gmail.com>
2020-11-05 23:28:57 +0100

    Day 54 (Bookworm project)

---
+++ Bookworm/Bookworm/AddBookView.swift
@@ -0,0 +1,65 @@
+//
+//  AddBookView.swift
+//  Bookworm
+//
+//  Created by denpazakura on 05/11/2020.
+//
+
+import SwiftUI
+
+struct AddBookView: View {
+    @Environment(\.managedObjectContext) var moc
+    @FetchRequest(entity: Book.entity(), sortDescriptors: []) var books: FetchedResults<Book>
+    
+    @State private var title = ""
+    @State private var author = ""
+    @State private var rating = 3
+    @State private var genre = ""
+    @State private var review = ""
+    
+    @State private var showingAddScreen = false
+    
+    let genres = ["Fantasy", "Horror", "Kids", "Mystery", "Poetry", "Romance", "Thriller"]
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Name of book", text: $title)
+                    TextField("Author's name", text: $author)
+                    
+                    Picker("Genre", selection: $genre) {
+                        ForEach(genres, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                }
+                
+                Section {
+                    RatingView(rating: $rating)
+                    TextField("Write a review", text: $review)
+                }
+                
+                Section {
+                    Button("Save") {
+                        let newBook = Book(context: self.moc)
+                        newBook.title = self.title
+                        newBook.author = self.author
+                        newBook.rating = Int16(self.rating)
+                        newBook.genre = self.genre
+                        newBook.review = self.review
+                        
+                        try? self.moc.save()
+                    }
+                }
+            }
+            .navigationBarTitle("Add Book")
+        }
+    }
+}
+
+struct AddBookView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddBookView()
+    }
+}
