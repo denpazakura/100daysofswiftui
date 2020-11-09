cac3b8faa25d5d9daa1cf1ee4a9e988b27dfdb88
denpazakura <swiftthesorrow@gmail.com>
2020-11-09 14:39:15 +0100

    Day 57 (CoreData project)

---
+++ CoreDataProject/CoreDataProject/ContentView.swift
@@ -0,0 +1,75 @@
+//
+//  ContentView.swift
+//  CoreDataProject
+//
+//  Created by denpazakura on 09/11/2020.
+//
+
+import SwiftUI
+import CoreData
+
+struct ContentView: View {
+    @Environment(\.managedObjectContext) private var viewContext
+    
+    @FetchRequest(entity: Movie.entity(), sortDescriptors: []) var movies: FetchedResults<Movie>
+
+    var body: some View {
+        List {
+            ForEach(movies) { movie in
+                Text(movie.title!)
+            }
+            .onDelete(perform: deleteItems)
+        }
+        .toolbar {
+            #if os(iOS)
+            EditButton()
+            #endif
+
+            Button(action: addItem) {
+                Label("Add Item", systemImage: "plus")
+            }
+        }
+    }
+
+    private func addItem() {
+        withAnimation {
+            
+            do {
+                try viewContext.save()
+            } catch {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                let nsError = error as NSError
+                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+            }
+        }
+    }
+
+    private func deleteItems(offsets: IndexSet) {
+        withAnimation {
+            offsets.map { movies[$0] }.forEach(viewContext.delete)
+
+            do {
+                try viewContext.save()
+            } catch {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                let nsError = error as NSError
+                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+            }
+        }
+    }
+}
+
+private let itemFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateStyle = .short
+    formatter.timeStyle = .medium
+    return formatter
+}()
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+    }
+}
---
+++ CoreDataProject/CoreDataProject/Movie/Movie+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Movie+CoreDataClass.swift
+//  CoreDataProject
+//
+//  Created by denpazakuras on 09/11/2020.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Movie)
+public class Movie: NSManagedObject {
+
+}
