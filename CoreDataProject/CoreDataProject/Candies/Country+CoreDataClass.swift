1f1ca290b3551fa046eb673402483d1e0712b7ed
denpazakura <swiftthesorrow@gmail.com>
2020-11-11 23:22:05 +0100

    Day 59 (CoreData project)

---
+++ CoreDataProject/CoreDataProject/Candies/Country+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Country+CoreDataClass.swift
+//  CoreDataProject
+//
+//  Created by denpazakura on 11/11/2020.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Country)
+public class Country: NSManagedObject {
+
+}
