017b57af4a96afc67cf406b37e278405802b4590
denpazakura <swiftthesorrow@gmail.com>
2020-11-22 21:17:09 +0100

    Day 68 (BucketList project)

---
+++ BucketList/BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Diana Komolova on 22/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
