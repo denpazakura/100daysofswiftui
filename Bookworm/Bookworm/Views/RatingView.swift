9dc5e5240a8d634a2836905cc864ea6b85bc116e
denpazakura <swiftthesorrow@gmail.com>
2020-12-11 23:59:11 +0100

    Day 75 (Bookworm project)

--- Bookworm/Bookworm/Views/RatingView.swift
+++ Bookworm/Bookworm/Views/RatingView.swift
@@ -9,14 +9,14 @@ import SwiftUI
 
 struct RatingView: View {
     @Binding var rating: Int
-
+    
     var label = ""
-
+    
     var maximumRating = 5
-
+    
     var offImage: Image?
     var onImage = Image(systemName: "star.fill")
-
+    
     var offColor = Color.gray
     var onColor = Color.yellow
     
@@ -25,13 +25,16 @@ struct RatingView: View {
             if label.isEmpty == false {
                 Text(label)
             }
-
+            
             ForEach(1..<maximumRating + 1) { number in
                 self.image(for: number)
                     .foregroundColor(number > self.rating ? self.offColor : self.onColor)
                     .onTapGesture {
                         self.rating = number
                     }
+                    .accessibility(label: Text("\(number == 1 ? "1 star" : "\(number) stars")"))
+                    .accessibility(removeTraits: .isImage)
+                    .accessibility(addTraits: number > self.rating ? .isButton : [.isButton, .isSelected])
             }
         }
     }
