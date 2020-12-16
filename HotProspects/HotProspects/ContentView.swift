44b9d1c2c6b296a4531101cee9bf0d471b9d6f44
denpazakura <swiftthesorrow@gmail.com>
2020-12-16 23:49:47 +0100

    Day 79 (HotProspects project)

---
+++ HotProspects/HotProspects/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HotProspects
+//
+//  Created by denpazakura on 16/12/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
