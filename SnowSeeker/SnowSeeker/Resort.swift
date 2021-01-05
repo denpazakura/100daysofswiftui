203aeeca101ef985cb96e383e171f66ee0aaf3b7
denpazakura <swiftthesorrow@gmail.com>
2021-01-05 20:54:40 +0100

    Day 97 (SnowSeeker project)

---
+++ SnowSeeker/SnowSeeker/Resort.swift
@@ -0,0 +1,20 @@
+//
+//  Resort.swift
+//  SnowSeeker
+//
+//  Created by denpazakura on 05/01/2021.
+//
+
+struct Resort: Codable, Identifiable {
+    let id: String
+    let name: String
+    let country: String
+    let description: String
+    let imageCredit: String
+    let price: Int
+    let size: Int
+    let snowDepth: Int
+    let elevation: Int
+    let runs: Int
+    let facilities: [String]
+}
